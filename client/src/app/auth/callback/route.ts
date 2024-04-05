8570a4462c3671b8fafd1f28473181ec5937e1ea
Shantanu <shantanuwable2003@gmail.com>
2024-04-05 09:07:07 +0530

    move changes

--- client/src/app/auth/callback/route.ts
+++ client/src/app/auth/callback/route.ts
@@ -1,7 +1,7 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-import type { Database } from "@/lib/supabase/database.types";
+import type { Database } from "@/supabase/database.types";
 
 export async function GET(request: NextRequest) {
     const requestUrl = new URL(request.url);
